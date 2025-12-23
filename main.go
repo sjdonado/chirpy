c43c510ca51b3195c48a53cdd8ff98145afdff21
Juan Rodriguez Donado <sjdonado@icloud.com>
2025-12-23 06:43:51 +0100

    feat: healthz and app paths

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"io"
 )
 
 func main() {
@@ -11,7 +12,14 @@ func main() {
 	filepathRoot := http.Dir(".")
 	port := "8080"
 
-	mux.Handle("/", http.FileServer(filepathRoot))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(filepathRoot)))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		if _, err := io.WriteString(w, "OK\n"); err != nil {
+			log.Fatal("Response could not be written")
+		}
+	})
 
 	s := &http.Server{
 		Addr: ":" + port,
