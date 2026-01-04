1b2c313a562bc20e159fe85e7c1da8d9e26afc36
Juan Rodriguez Donado <sjdonado@icloud.com>
2026-01-04 11:47:28 +0100

    fix: validate env variables are defined

--- internal/api/config.go
+++ internal/api/config.go
@@ -16,10 +16,30 @@ type Config struct {
 func NewConfig() *Config {
 	godotenv.Load()
 
+	DBURL := os.Getenv("DB_URL")
+	if DBURL == "" {
+		panic("DB_URL environment variable is not set")
+	}
+
+	platform := os.Getenv("PLATFORM")
+	if platform == "" {
+		panic("PLATFORM environment variable is not set")
+	}
+
+	JWTSecret := os.Getenv("JWT_SECRET")
+	if JWTSecret == "" {
+		panic("JWT_SECRET environment variable is not set")
+	}
+
+	polkaKey := os.Getenv("POLKA_KEY")
+	if polkaKey == "" {
+		panic("POLKA_KEY environment variable is not set")
+	}
+
 	return &Config{
-		DBURL:     os.Getenv("DB_URL"),
-		Platform:  os.Getenv("PLATFORM"),
-		JWTSecret: os.Getenv("JWT_SECRET"),
-		PolkaKey:  os.Getenv("POLKA_KEY"),
+		DBURL:     DBURL,
+		Platform:  platform,
+		JWTSecret: JWTSecret,
+		PolkaKey:  polkaKey,
 	}
 }
